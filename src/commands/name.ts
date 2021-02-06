395abc2341fbef05c916abc54fd06b1168221887
Elantris <j88107333@gmail.com>
2021-02-07 02:23:16 +0800

    Add command name

---
+++ src/commands/name.ts
@@ -0,0 +1,23 @@
+import database, { cache } from '../utils/database'
+import { CommandProps } from '../utils/types'
+
+const commandName: CommandProps = async ({ message, args }) => {
+  if (args.length === 0) {
+    return {
+      content: ':triangular_flag_on_post: USER_TAG 的顯示名稱為：NICKNAME'
+        .replace('USER_TAG', message.author.tag)
+        .replace('NICKNAME', cache.names[message.author.id] || message.member?.displayName || ''),
+    }
+  }
+
+  const newName = args.join('')
+  await database.ref(`/names/${message.author.id}`).set(newName)
+
+  return {
+    content: ':triangular_flag_on_post: USER_TAG 的顯示名稱已改為：NICKNAME'
+      .replace('USER_TAG', message.author.tag)
+      .replace('NICKNAME', newName),
+  }
+}
+
+export default commandName
