70b8709b3b9f39ad882d842fa461908d83e11035
Elantris <j88107333@gmail.com>
2021-02-16 04:56:24 +0800

    Fix filter of roles in command report

--- src/commands/report.ts
+++ src/commands/report.ts
@@ -1,8 +1,8 @@
+import { EmbedFieldData } from 'discord.js'
 import moment from 'moment'
 import { CommandProps } from '../types'
 import database, { cache } from '../utils/database'
 import isValidDate from '../utils/isValidDate'
-import searchMembers from '../utils/searchMembers'
 
 const commandReport: CommandProps = async ({ message, guildId, args }) => {
   if (!message.member?.hasPermission('ADMINISTRATOR')) {
@@ -46,8 +46,8 @@ const commandReport: CommandProps = async ({ message, guildId, args }) => {
   ).val()
   await loadingMessage.delete()
 
-  const dates = Object.keys(rawData)
-  if (dates.length === 0) {
+  const recordDates = Object.keys(rawData)
+  if (recordDates.length === 0) {
     return {
       content: ':x: 這段時間內沒有紀錄',
     }
@@ -59,38 +59,75 @@ const commandReport: CommandProps = async ({ message, guildId, args }) => {
       count: number
     }
   } = {}
-  for (const i in rawData) {
-    const v = rawData[i].split(' ')
-    for (const memberId of v) {
-      if (!attendedMembers[memberId]) {
-        attendedMembers[memberId] = {
-          name: cache.names[memberId] || (await searchMembers(message, [memberId]))[0]?.displayName || memberId,
-          count: 1,
+
+  await message.guild?.roles.fetch()
+  const targetRoles = message.guild?.roles.cache.filter(role => cache.settings[guildId]?.roles.includes(role.id))
+  const isEveryone = targetRoles?.size === 0
+  if (isEveryone) {
+    await message.guild?.members.fetch()
+    message.guild?.members.cache
+      .filter(member => !member.user.bot)
+      .forEach(member => {
+        attendedMembers[member.id] = {
+          name: cache.names[member.id] || member.displayName.slice(0, 20),
+          count: 0,
         }
-      } else {
-        attendedMembers[memberId].count += 1
-      }
-    }
+      })
+  } else {
+    targetRoles?.forEach(role => {
+      role.members
+        .filter(member => !member.user.bot || !!attendedMembers[member.id])
+        .forEach(member => {
+          attendedMembers[member.id] = {
+            name: cache.names[member.id] || member.displayName.slice(0, 20),
+            count: 0,
+          }
+        })
+    })
   }
+
+  Object.values(rawData).forEach(v => {
+    v.split(' ')
+      .filter(memberId => !!attendedMembers[memberId])
+      .forEach(memberId => {
+        attendedMembers[memberId].count += 1
+      })
+  })
+
+  const fields: EmbedFieldData[] = []
+  new Array(isEveryone ? recordDates.length : recordDates.length + 1).fill(0).forEach((_, i) => {
+    const filteredMemberIds = Object.keys(attendedMembers)
+      .filter(memberId => attendedMembers[memberId].count === recordDates.length - i)
+      .sort()
+    const memberCount = filteredMemberIds.length
+    while (filteredMemberIds.length) {
+      fields.push({
+        name: filteredMemberIds.length === memberCount ? `出席 ${recordDates.length - i} 次：${memberCount} 人` : '.',
+        value: filteredMemberIds
+          .splice(0, 30)
+          .map(memberId => attendedMembers[memberId].name)
+          .join('、'),
+      })
+    }
+  })
 
   return {
     content: ':triangular_flag_on_post: 出席統計 `START_DATE` ~ `END_DATE`'
       .replace('START_DATE', startDate)
       .replace('END_DATE', endDate),
     embed: {
-      fields: [
-        {
-          name: `點名紀錄：${dates.length} 次`,
-          value: dates.map(date => `\`${date}\``).join(' '),
-        },
-        ...new Array(dates.length).fill(0).map((_, i) => ({
-          name: `出席 ${dates.length - i} 次`,
-          value: Object.keys(attendedMembers)
-            .filter(memberId => attendedMembers[memberId].count === dates.length - i)
-            .map(memberId => attendedMembers[memberId].name)
-            .join('、'),
-        })),
-      ],
+      description: '點名紀錄：DATES\n目標身份組：ROLES'
+        .replace('DATES', recordDates.map(date => `\`${date}\``).join(' '))
+        .replace(
+          'ROLES',
+          isEveryone
+            ? '所有人'
+            : message.guild?.roles.cache
+                .filter(role => cache.settings[guildId].roles.includes(role.id))
+                .map(role => role.name)
+                .join(' ') || '',
+        ),
+      fields,
     },
   }
 }
